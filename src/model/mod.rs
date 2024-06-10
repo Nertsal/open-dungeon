4d9329ff1add8fb42d5cb1956e5b83f0e8b58d47
nertsal <nestralnertsal@gmail.com>
2024-06-10 22:56:41 +0300

    colliders code

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,4 +1,10 @@
-use geng::prelude::*;
+mod collider;
+
+use crate::prelude::*;
+
+pub type Camera = Camera2d;
+pub type Coord = R32;
+pub type Position = vec2<Coord>;
 
 pub struct Model {}
 
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,3 @@
 pub use crate::{assets::*, model::*};
 pub use geng::prelude::*;
+pub use geng_utils::conversions::*;
